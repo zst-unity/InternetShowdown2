4ed85be4a56c3c04a68f4ff7456ffe6a56b6a739
zstardustttt <93951756+zSt4rdust@users.noreply.github.com>
2024-05-07 01:30:06 +0700

    more reset

--- Assets/_Project/Scripts/Systems/Reset.cs
+++ Assets/_Project/Scripts/Systems/Reset.cs
@@ -1,15 +1,29 @@
 using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
 
 namespace InternetShowdown.Systems
 {
     // нужна чтобы сбрасывать поля ассетов при остановке плеймода (чтобы гит не видел изменения бесполезные)
     public class Reset : LocalSystem<Reset>
     {
+        [SerializeField] private UniversalRendererData _rendererData;
+        [SerializeField] private VolumeProfile _volumeProfile;
+        [SerializeField] private UniversalRenderPipelineAsset _renderPipelineAsset;
         [SerializeField] private Material _speedlines;
 
         protected override void OnStop()
         {
             _speedlines.SetFloat("_Alpha", 0f);
+            foreach (var rendererFeature in _rendererData.rendererFeatures)
+            {
+                rendererFeature.SetActive(true);
+            }
+            foreach (var component in _volumeProfile.components)
+            {
+                component.active = true;
+            }
+            _renderPipelineAsset.renderScale = 1f;
         }
     }
 }
