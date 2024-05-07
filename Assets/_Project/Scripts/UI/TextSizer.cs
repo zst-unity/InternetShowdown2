9a67aee6a043c1c8b5b81ce1ddea0105456ec9fa
zstardustttt <93951756+zSt4rdust@users.noreply.github.com>
2024-05-07 23:18:49 +0700

    menu design full

---
+++ Assets/_Project/Scripts/UI/TextSizer.cs
@@ -0,0 +1,116 @@
+using System;
+using System.Linq;
+using TMPro;
+using UnityEditor;
+using UnityEngine;
+
+namespace InternetShowdown.UI
+{
+    [ExecuteAlways]
+    public class TextSizer : MonoBehaviour
+    {
+        public bool ResizeTextObject = true;
+        public Vector2 Padding;
+        public Vector2 MaxSize = new(1000, float.PositiveInfinity);
+        public Vector2 MinSize;
+        public Mode ControlAxes = Mode.Both;
+
+        [Flags]
+        public enum Mode
+        {
+            None = 0,
+            Horizontal = 0x1,
+            Vertical = 0x2,
+            Both = Horizontal | Vertical
+        }
+
+        private RectTransform _selfRectTransform;
+
+        protected virtual float MinX
+        {
+            get
+            {
+                if ((ControlAxes & Mode.Horizontal) != 0) return MinSize.x;
+                return _selfRectTransform.rect.width - Padding.x;
+            }
+        }
+        protected virtual float MinY
+        {
+            get
+            {
+                if ((ControlAxes & Mode.Vertical) != 0) return MinSize.y;
+                return _selfRectTransform.rect.height - Padding.y;
+            }
+        }
+        protected virtual float MaxX
+        {
+            get
+            {
+                if ((ControlAxes & Mode.Horizontal) != 0) return MaxSize.x;
+                return _selfRectTransform.rect.width - Padding.x;
+            }
+        }
+        protected virtual float MaxY
+        {
+            get
+            {
+                if ((ControlAxes & Mode.Vertical) != 0) return MaxSize.y;
+                return _selfRectTransform.rect.height - Padding.y;
+            }
+        }
+
+        protected virtual void UpdateElement()
+        {
+            var texts = GetComponentsInChildren<TMP_Text>().Where(text => text.gameObject != gameObject && text.transform.parent == transform).ToArray();
+
+            var widestPreferredSize = Vector2.zero;
+            foreach (var text in texts)
+            {
+                var preferredSize = text.GetPreferredValues(MaxX, MaxY);
+                preferredSize.x = Mathf.Clamp(preferredSize.x, MinX, MaxX);
+                preferredSize.y = Mathf.Clamp(preferredSize.y, MinY, MaxY);
+                preferredSize += Padding;
+
+                if (preferredSize.x > widestPreferredSize.x) widestPreferredSize.x = preferredSize.x;
+                if (preferredSize.y > widestPreferredSize.y) widestPreferredSize.y = preferredSize.y;
+
+                if ((ControlAxes & Mode.Horizontal) != 0)
+                {
+                    _selfRectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, widestPreferredSize.x);
+                    if (ResizeTextObject)
+                    {
+                        text.rectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, preferredSize.x);
+                    }
+                }
+                if ((ControlAxes & Mode.Vertical) != 0)
+                {
+                    _selfRectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, widestPreferredSize.y);
+                    if (ResizeTextObject)
+                    {
+                        text.rectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, preferredSize.y);
+                    }
+                }
+            }
+        }
+
+#if UNITY_EDITOR
+        private void OnEnable()
+        {
+            ObjectChangeEvents.changesPublished += ChangesPublished;
+        }
+
+        private void OnDisable()
+        {
+            ObjectChangeEvents.changesPublished -= ChangesPublished;
+        }
+
+        private void ChangesPublished(ref ObjectChangeEventStream stream)
+        {
+            for (int i = 0; i < stream.length; ++i)
+            {
+                UpdateElement();
+            }
+        }
+#endif
+    }
+}
